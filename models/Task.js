4d593a731870439c1f99000076b45fb92c092070
shobhitjain <jshobhit3020@gmail.com>
2024-09-07 22:13:57 +0530

    first commit

--- models/Task.js
+++ models/Task.js
@@ -1,16 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true // Make sure the task must be associated with a user
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    dueDate: { type: Date },
+    status: {
+      type: String,
+      enum: ["Pending", "Completed"],
+      default: "Pending",
+    },
   },
-  title: { type: String, required: true },
-  description: { type: String },
-  priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Medium' },
-  dueDate: { type: Date },
-  status: { type: String, enum: ['Pending', 'Completed'], default: 'Pending' },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
